9606caf7434d08d0634fec87ed9eaf0439bba522
Guilherme Coelho <guilhermeismco@gmail.com>
2015-11-01 18:23:18 -0500

    Added the next level triggers

---
+++ One Dimension Mario/Assets/goLevel4.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class goLevel4 : MonoBehaviour {
+
+	bool nextStage = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (nextStage == true) {
+			Application.LoadLevel("Paul Level 4");
+		}
+	}
+
+	void OnTriggerEnter(Collider other){
+		Debug.Log ("hit");
+		nextStage = true;
+	}
+}
---
+++ One Dimension Mario/Assets/level4.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class level4 : MonoBehaviour {
+
+	bool nextStage = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (nextStage == true) {
+			Application.LoadLevel ("Paul level 3");
+		}
+	
+	}
+
+	void OnTriggerEnter(Collider other){
+		Debug.Log ("hit");
+		nextStage = true;
+	}
+}
