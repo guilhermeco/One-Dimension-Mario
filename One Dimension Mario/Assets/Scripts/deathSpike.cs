43192f5511113bba497f67652a4f08b5f985c007
Luis Gerardo Huiza Blanco <LGHUIZA@Luiss-MacBook-Pro.local>
2015-11-02 11:14:15 -0500

    Update

---
+++ One Dimension Mario/Assets/Scripts/deathSpike.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class deathSpike : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter (Collider col) {
+
+		if (col.gameObject.name == "Player") {
+
+			int currentLevel = Application.loadedLevel;
+			Debug.Log (currentLevel);
+		
+			Debug.Log ("REKT");
+		
+			Destroy (col.gameObject);
+		
+			Application.LoadLevel (currentLevel);
+		}
+	}
+}
