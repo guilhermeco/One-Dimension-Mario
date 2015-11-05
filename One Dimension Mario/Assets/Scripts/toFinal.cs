33ab38fda0c8a893e4d6b6ccd75251b8f720fdb1
Guilherme Coelho <guilhermeismco@gmail.com>
2015-11-05 02:03:42 -0500

    level 5 next level trigger

---
+++ One Dimension Mario/Assets/Scripts/toFinal.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class toFinal : MonoBehaviour {
+
+	bool nextStage = false;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (nextStage == true) {
+			Application.LoadLevel ("Final Scene");
+		}
+		
+	}
+	
+	void OnTriggerEnter(Collider other){
+		Debug.Log ("hit");
+		nextStage = true;
+	}
+}
