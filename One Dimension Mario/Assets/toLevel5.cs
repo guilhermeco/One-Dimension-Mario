55b8a1707a315acb021fb91b52e0561bf49c1379
Guilherme Coelho <guilhermeismco@gmail.com>
2015-11-05 01:18:08 -0500

    more triggers

---
+++ One Dimension Mario/Assets/toLevel5.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class toLevel5 : MonoBehaviour {
+
+	bool nextStage = false;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (nextStage == true) {
+			Application.LoadLevel ("Paul level 5");
+		}
+		
+	}
+	
+	void OnTriggerEnter(Collider other){
+		Debug.Log ("hit");
+		nextStage = true;
+	}
+}
